9e56c73a6d6dd88d8b1c2413ab0f755bdb0a2430
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-07-31 10:25:20 -0300

    meti dotenv en el infura.js

--- frontend/src/config/Infura.mjs
+++ frontend/src/config/Infura.mjs
@@ -1,14 +1,14 @@
 
 // import '../../globals.js';
 // import 'react-native-url-polyfill/auto.js'
-let REACT_APP_PROJECT_ID = "2PTeXaWpD6pJoeAO7L7Up1eH9ID"
-let REACT_APP_PROJECT_SECRET_KEY = "1bec7aa48ccc32e3fa7d63ae74ed2940"
+// let REACT_APP_PROJECT_ID = "2PTeXaWpD6pJoeAO7L7Up1eH9ID"
+// let REACT_APP_PROJECT_SECRET_KEY = "1bec7aa48ccc32e3fa7d63ae74ed2940"
+import 'dotenv/config'
 import { create as ipfsHttpClient } from 'ipfs-http-client';
 import { Buffer } from 'buffer';
-// import {REACT_APP_PROJECT_ID, REACT_APP_PROJECT_SECRET_KEY} from '@env'
 
-const projectId = REACT_APP_PROJECT_ID; 
-const projectSecretKey = REACT_APP_PROJECT_SECRET_KEY;
+const projectId = process.env.REACT_APP_PROJECT_ID; 
+const projectSecretKey = process.env.REACT_APP_PROJECT_SECRET_KEY;
 const auth = `Basic ${Buffer.from(`${projectId}:${projectSecretKey}`).toString(
     "base64"
 )}`
